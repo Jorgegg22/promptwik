88d72ba457197794c98004a2311654747b110fc3
Jorge Gómez Garcia <jgg2232004@gmail.com>
2026-03-31 17:25:48 +0200

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://promptwik.com',
+  site: "https://promptwik.com",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
 });
